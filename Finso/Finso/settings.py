7c0bcf9a635ae7670fd9e78972703fc6a7336e15
Jishan5707 <jishanp5707@gmail.com>
2024-02-27 12:33:07 +0530

    Added Index Page

--- Finso/Finso/settings.py
+++ Finso/Finso/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Finso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
